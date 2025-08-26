366f2ce032e55e2708000aa0c5786fd2bd470d8f
Md. Minhaz Uddin Sadi <infosadi243204@gmail.com>
2025-08-26 23:34:17 +0600

    Add files via upload

---
+++ Problem_6.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Problem_6 {
+    
+    public static void main(String[] args){
+        
+        Scanner input = new Scanner(System.in);
+
+       
+        int N = input.nextInt();
+
+        for(int i=1; i<=N; i++){
+            System.out.print(i+" ");
+        }
+
+        input.close();
+
+    }
+}
