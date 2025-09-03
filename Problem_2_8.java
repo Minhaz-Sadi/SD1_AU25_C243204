cac985baa8435ed04cd0efffc03b7120a3d4ccd4
Md. Minhaz Uddin Sadi <infosadi243204@gmail.com>
2025-09-03 21:59:34 +0600

    Add files via upload

---
+++ Problem_2_8.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Problem_2_8 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        String st = input.nextLine();
+
+        String rev = "";
+        for (int i = st.length() - 1; i >= 0; i--) {
+            rev += st.charAt(i);
+        }
+        // System.out.println(st);
+        // System.out.println(rev);
+        int f = 0;
+        for (int i = st.length() - 1; i >= 0; i--) {
+            if (st.charAt(i) != rev.charAt(i)) {
+                f = 1;
+                break;
+            }
+        }
+        if (f == 0)
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not Palindrome");
+        input.close();
+
+    }
+}
